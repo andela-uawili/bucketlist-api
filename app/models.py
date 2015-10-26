84c9a5ca8b68da5f041312ea2123696cdd7ad37d
Uzo Awili <uzo.awili@andela.com>
2015-10-26 03:13:40 +0100

    Corrected date_joined field on User model

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ class User(db.Model):
     email = db.Column(db.Text, index=True, unique=True)
     password_hash = db.Column(db.Text)
     username = db.Column(db.Text, nullable=True)
-    date_joined = db.Column(db.DateTime, index=True, default=datetime.now)
+    date_joined = db.Column(db.DateTime, index=True, default=datetime.now())
     logged_in = db.Column(db.Boolean, default=False)
 
     bucketlists = db.relationship(
